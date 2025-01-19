ae02af2dc5c303b6488b58c94b722e18bd130089
Minh Tuan Pham <minhtuan0295@gmail.com>
2025-01-19 18:21:33 +0100

    Create src folder and move server_app.py to src folder

--- src/server_app.py
+++ src/server_app.py
@@ -1,9 +1,12 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import platform
+from datetime import datetime
 
 hostName = "0.0.0.0"
-serverPort = 8080
+serverPort = 10000
+
+current_time = datetime.now().strftime("%M:%H %d/%m/%Y")
+current_time_print_msg = "<p>Today is" + current_time + "</p>"
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -11,13 +14,12 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is a web server developed by Minh Tuan Pham</p>", "utf-8"))
-        self.wfile.write(bytes("<p>Today is 01.01.2025</p>", "utf-8"))
+        self.wfile.write(bytes("<p>This is a test web server</p>", "utf-8"))
+        self.wfile.write(bytes(current_time_print_msg, "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
-    print(platform.python_version())
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
